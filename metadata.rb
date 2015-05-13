f11538e72633392c971becee53460c245be39294
EA <ea@ea-imac>
2015-05-13 16:54:01 +0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'blueleaf-chef'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures blueleaf-chef'
+long_description 'Installs/Configures blueleaf-chef'
+version          '0.1.0'
